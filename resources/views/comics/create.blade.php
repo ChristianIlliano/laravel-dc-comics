ec02755f8300c107d22bed015d33bdc94d198bdd
ChristianIlliano <grafica.illiano@gmail.com>
2024-06-14 22:41:19 +0200

    feat: fillable

---
+++ resources/views/comics/create.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section("content")
+    <div class="container">
+        <h1>Aggiungi un nuovo fumetto</h1>
+
+        <form action="{{ route('comics.store') }}" method="POST">
+            {{-- Cookie per far riconoscere il form al server --}}
+            @csrf
+
+            <div class="mb-3">
+                <label for="title" class="form-label">Titolo</label>
+                <input type="text" class="form-control" id="title" name="title">
+            </div>
+
+            <div class="mb-3">
+                <label for="type" class="form-label">Tipologia</label>
+                <select class="form-select" id="type" name="type">
+                    <option selected>Seleziona</option>
+                    <option value="corta">Graphic novel</option>
+                    <option value="cortissima">Comic Book</option>
+                </select>
+            </div>
+
+            <div class="mb-3">
+                <label for="weight" class="form-label">Serie</label>
+                <input type="text" class="form-control" id="series" name="series">
+            </div>
+
+            <div class="mb-3">
+                <label for="image" class="form-label">Image</label>
+                <input type="text" class="form-control" id="thumb" name="thumb">
+            </div>
+
+            <div class="mb-3">
+                <label for="description" class="form-label">Descrizione</label>
+                <textarea class="form-control" id="description" name="description" rows="3"></textarea>
+            </div>
+
+            <button class="btn btn-success" type="submit">Salva</button>
+        </form>
+    </div>
+@endsection
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,28 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1>Lista Fumetti</h1>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">Serie</th>
-                    <th scope="col">Titolo</th>
-                    <th scope="col">Tipologia</th>
-                    <th scope="col">Prezzo</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($listaComics as $item)
-                <tr>
-                    <th scope="row">{{ $item->series }}</th>
-                    <td>{{ $item->title }}</td>
-                    <td>{{ $item->type }}</td>
-                    <td>{{ $item->price }}</td>
-                    <td><a href="{{route("comics.show",["comic"=>$item->id])}}">Dettagli</a></td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+<div class="container">
+    <h1>Lista Fumetti</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Serie</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Tipologia</th>
+                <th scope="col">Prezzo</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($listaComics as $item)
+            <tr>
+                <th scope="row">{{ $item->series }}</th>
+                <td>{{ $item->title }}</td>
+                <td>{{ $item->type }}</td>
+                <td>{{ $item->price }}</td>
+                <td><a href="{{route("comics.show",["comic"=>$item->id])}}">Dettagli</a></td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    <a type="button" class="btn btn-primary" href="{{route("comics.create")}}">Aggiungi un fumetto</a>
     </div>
 @endsection
