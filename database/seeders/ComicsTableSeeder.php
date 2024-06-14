71155ffec485229262fe31ac96b443391afac72c
ChristianIlliano <grafica.illiano@gmail.com>
2024-06-14 17:01:46 +0200

    feat: seeder in db

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Comic;
 
 class ComicsTableSeeder extends Seeder
 {
@@ -12,6 +13,17 @@ class ComicsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        
+        $data = config("comic");
+        foreach($data as $item) {
+            $newComic = new Comic();
+            $newComic->title = $item["title"];
+            $newComic->description = $item["description"];
+            $newComic->thumb = $item["thumb"];
+            $newComic->price = $item["price"];
+            $newComic->series = $item["series"];
+            $newComic->sale_date = $item["sale_date"];
+            $newComic->type = $item["type"];
+            $newComic->save();
+        }
     }
 }
