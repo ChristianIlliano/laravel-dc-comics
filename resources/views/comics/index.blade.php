2bfc6bd78b53a54281a02d706b65fb73039cb7bf
ChristianIlliano <grafica.illiano@gmail.com>
2024-06-14 22:23:43 +0200

    feat: details index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -19,19 +19,9 @@
                     <td>{{ $item->title }}</td>
                     <td>{{ $item->type }}</td>
                     <td>{{ $item->price }}</td>
+                    <td><a href="{{route("comics.show",["comic"=>$item->id])}}">Dettagli</a></td>
                 </tr>
                 @endforeach
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
-                    <td>@twitter</td>
-                </tr>
             </tbody>
         </table>
     </div>
